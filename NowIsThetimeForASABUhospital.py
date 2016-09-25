5a3db0588d55e3fdfe7e71fe385430e8c46abd83
tomokitamaki <godspeeed9@gmail.com>
2016-09-25 19:06:55 +0900

    正規表現の検索を辞めました。
    正規表現でなくて単純なマッチングでいけるのでやめました。
    また、マッチングしなかったときの戻り値が-1で、0以上であれば1HIT以上はしたということで
    if文の判定の仕方を変えています。

--- NowIsThetimeForASABUhospital.py
+++ NowIsThetimeForASABUhospital.py
@@ -1,7 +1,6 @@
 #! /root/.pyenv/shims/python3.5
 
 import requests
-import re
 from bs4 import BeautifulSoup
 import json
 import time
@@ -14,14 +13,16 @@ f.close
 
 # 診察番号を引数で取得するようなコードを書くところから
 
+search_num = str('[' + args[1] + ']')
 cnt = 0
 while (cnt < 5):
     r = requests.get('http://konzatsu.net/sfd-m.php?cid=17514799')
-    result = re.findall(args[1], r.text)
-    if not result:
+    result = r.text.find(search_num)
+    if result < 0:
         print("Empty\"r\"")
         if not cnt == 0:
              sys.exit()
+        time.sleep(300)
     else:
         payload_dic = {
             "text":'もうすぐ診察の順番ですよ',
